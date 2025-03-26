90e122f34f33b19b91d9831f74e5c60d98b5d7fe
MariEDaitx <mariaeduardasantosdaitx@gmail.com>
2025-03-26 15:08:36 -0300

    adicionando o requirimente.txt

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import sqlite3
 from flask import Flask, request, jsonify # type: ignore
-
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 def int_db():
     with sqlite3.connect("database.db") as conn:
         conn.execute(
